797867c9c54ded79d3ac37efde12c76cf5554dbc
VIPLODER0 <ankush21012005@gmail.com>
2026-07-08 10:34:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,9 @@ API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-BOT2_TOKEN = os.getenv("BOT2_TOKEN", "")
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/secret_share_bot_default_db")
-
+BOT2_TOKEN = os.getenv("BOT2_TOKEN", "")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
